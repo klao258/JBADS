2908b47643da5656862e89a79a28de9e2f5a9ba3
klao258 <klao258@users.noreply.github.com>
2025-05-24 18:31:46 +0000

    deploy: 560ae8b5f6f28c419d94e0892e95a30e86992e87

--- tg-ads.user.js
+++ tg-ads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TG广告发布自动化脚本
 // @namespace    https://klao258.github.io/
-// @version      2025.05.25-02:29:02
+// @version      2025.05.25-02:31:46
 // @description  Telegram ADS 自动发布辅助工具，支持结构注入、页面监听、数据联动等功能
 // @author       You
 // @match        https://ads.telegram.org/*
@@ -195,7 +195,7 @@
             ];
 
             // 加载 autoADSData
-            const ready = await loadMultipleScriptsAndWaitForAll(scripts, ['window.autoADSData']);
+            const ready = await loadMultipleScriptsAndWaitForAll(scripts, ['autoADSData']);
 
             // 加载 postData
             const expectedVars = [
