a1d6275a9be68a03cf4220fedd63b3495ffc02e6
klao258 <klao25388@gmail.com>
2025-05-25 03:10:23 +0800

    不用更新版本

--- autoADS.js
+++ autoADS.js
@@ -1,6 +1,7 @@
 (() => {
     "use strict";
-
+    console.log('不用更新版本')
+    
     const { accountObj, FTChannel, JBChannel, DBChannel, ADSChannels, promoteOpts, GQText, copyText, guid, getRNum, sleep, date, timestampToDate, inStr } = autoADSData;
 
     window.isLoad = false;
