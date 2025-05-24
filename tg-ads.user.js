9a63e2ce3a1c8d9abd0fe43984f8d100cd524768
klao258 <klao258@users.noreply.github.com>
2025-05-24 14:20:54 +0000

    deploy: 897f473d51452b9e043cd089841509bff14ae1be

--- tg-ads.user.js
+++ tg-ads.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         TG广告发布自动化脚本
 // @namespace    https://klao258.github.io/
-// @version      2025.05.24-21:49:42
+// @version      2025.05.24-22:20:54
 // @description  Telegram ADS 自动发布辅助工具，支持结构注入、页面监听、数据联动等功能
 // @author       You
 // @match        https://ads.telegram.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=telegram.org
 // @updateURL    https://klao258.github.io/JBADS/tg-ads.user.js
 // @downloadURL  https://klao258.github.io/JBADS/tg-ads.user.js
-// @require      https://klao258.github.io/JBADS/adsData/ZhaoShang.js
 // @require      https://klao258.github.io/JBADS/autoADSData.js
 // @require      https://klao258.github.io/JBADS/autoADS.js
 // @grant        GM_addStyle
