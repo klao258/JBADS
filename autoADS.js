19748cdb52cbedd82c685bb339bb3e8b6f352e80
klao258 <klao25388@gmail.com>
2025-05-25 22:41:08 +0800

    优化

--- autoADS.js
+++ autoADS.js
@@ -1423,10 +1423,10 @@
                     }
 
                     // 分数都为0，用 pays 字段排序（升序）
-                    const aJonins = a?.joins || 0;
-                    const bJonins = b?.joins || 0;
+                    const aPviews = a?.pviews || 0;
+                    const bPviews= b?.pviews || 0;
 
-                    return bJonins - aJonins;
+                    return bPviews - aPviews;
 
                     // (b.score - a.score) || (b.pays - a.pays))
                 });
