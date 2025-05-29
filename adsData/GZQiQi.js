e05601ef0966a22f6b0faedd195d9f8edfe3cb89
klao258 <klao258@users.noreply.github.com>
2025-05-29 17:46:20 +0000

    deploy: 956cf7a01c683ffc68f97dbddf2ef189415d74d5

--- adsData/GZQiQi.js
+++ adsData/GZQiQi.js
@@ -1,15 +1,15 @@
 var postData = {
-  "ADS-GZQiQi-18V6L6jK6O": "16-1-610",
+  "ADS-GZQiQi-18V6L6jK6O": "18-1-610",
   "ADS-GZQiQi-18jNfn3HhG": "7-1-475.00",
-  "ADS-GZQiQi-18f6oPJxG8": "13-0-0",
-  "ADS-GZQiQi-18E9k5DFXj": "13-1-43.00",
+  "ADS-GZQiQi-18f6oPJxG8": "15-0-0",
+  "ADS-GZQiQi-18E9k5DFXj": "14-1-43.00",
   "ADS-GZQiQi-181H93xzpO": "4-0-0",
   "ADS-GZQiQi-18wyPPWLLK": "10-0-0",
-  "ADS-GZQiQi-18wn7KJH3t": "7-0-0",
+  "ADS-GZQiQi-18wn7KJH3t": "10-0-0",
   "ADS-GZQiQi-18Ny7sD2fG": "15-0-0",
-  "ADS-GZQiQi-18MZqE9GlF": "13-0-0",
-  "ADS-GZQiQi-18HV1L5OO2": "3-0-0",
-  "ADS-GZQiQi-18c35bpWj6": "3-0-0",
+  "ADS-GZQiQi-18MZqE9GlF": "15-0-0",
+  "ADS-GZQiQi-18HV1L5OO2": "4-0-0",
+  "ADS-GZQiQi-18c35bpWj6": "4-0-0",
   "ADS-GZQiQi-18xn7rHQla": "2-0-0",
   "ADS-GZQiQi-18BRPW8NZs": "2-0-0"
 }
