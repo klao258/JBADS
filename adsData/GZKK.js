ec7ae3c9d10494ca38ea701bbb4d82d51a76cf6a
klao258 <klao258@users.noreply.github.com>
2025-06-05 09:19:38 +0000

    deploy: 21eca762a4d30482844190b711d010f3825b79a4

--- adsData/GZKK.js
+++ adsData/GZKK.js
@@ -1,12 +1,12 @@
 var postData = {
   "ADS-GZKK-101XArBSiy": "50-5-223.4",
   "ADS-GZKK-10Y3jb8WoN": "9-1-88.6",
-  "ADS-GZKK-10H12CHCEB": "28-0-0",
-  "ADS-GZKK-10KVMopf1i": "32-0-0",
-  "ADS-GZKK-10HRfYWtEi": "35-0-0",
+  "ADS-GZKK-10H12CHCEB": "31-0-0",
+  "ADS-GZKK-10KVMopf1i": "34-0-0",
+  "ADS-GZKK-10HRfYWtEi": "36-0-0",
   "ADS-GZKK-10upIBD8DP": "9-0-0",
-  "ADS-GZKK-10X1AgF0vQ": "35-1-100",
-  "ADS-GZKK-10H5iFuPa5": "20-0-0",
+  "ADS-GZKK-10X1AgF0vQ": "36-1-100",
+  "ADS-GZKK-10H5iFuPa5": "21-0-0",
   "ADS-GZKK-10iP0vxXgr": "1-0-0",
-  "ADS-GZKK-10SPy62YgD": "5-0-0"
+  "ADS-GZKK-10SPy62YgD": "6-0-0"
 }
