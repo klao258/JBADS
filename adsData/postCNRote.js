e19af7b56347cf618409dbb3042b7d261ed3c7a0
klao258 <klao25388@gmail.com>
2025-06-12 16:20:10 +0800

    更新数据

--- adsData/postCNRote.js
+++ adsData/postCNRote.js
@@ -2,18 +2,18 @@ var postCNRote = {
   "GZKK": {
     "ADS-GZKK-10H12CHCEB": "10-24-70.59%",
     "ADS-GZKK-10X1AgF0vQ": "14-25-64.10%",
-    "ADS-GZKK-10KVMopf1i": "17-17-50.00%",
-    "ADS-GZKK-10HRfYWtEi": "22-21-48.84%",
+    "ADS-GZKK-10KVMopf1i": "17-18-51.43%",
+    "ADS-GZKK-10HRfYWtEi": "23-21-47.73%",
     "ADS-GZKK-101XArBSiy": "33-17-34.00%"
   },
   "ZhaoShang": {
     "ADS-ZhaoShang-0jt85Sndi": "47-114-70.81%",
-    "ADS-ZhaoShang-0JveORGv9": "50-80-61.54%",
+    "ADS-ZhaoShang-0JveORGv9": "50-81-61.83%",
     "ADS-ZhaoShang-0gGRsxL0E": "17-23-57.50%",
     "ADS-ZhaoShang-0YVbpiAGo": "49-59-54.63%",
     "ADS-ZhaoShang-00s0C2q1t": "32-38-54.29%",
+    "ADS-ZhaoShang-0apBDDgB2": "39-45-53.57%",
     "ADS-ZhaoShang-0396oZA8X": "38-43-53.09%",
-    "ADS-ZhaoShang-0apBDDgB2": "39-43-52.44%",
     "ADS-ZhaoShang-044W8jVyE": "16-16-50.00%",
     "ADS-ZhaoShang-0peEen4dE": "66-65-49.62%",
     "ADS-ZhaoShang-0asmFb9MA": "38-35-47.95%",
@@ -22,35 +22,35 @@ var postCNRote = {
   },
   "TouTou": {
     "ADS-TouTou-4f0zhiu6O": "10-32-76.19%",
-    "ADS-TouTou-4XQ7dJsxr": "179-110-38.06%"
+    "ADS-TouTou-4XQ7dJsxr": "179-111-38.28%"
   },
   "JingJing": {
-    "ADS-JingJing-1nkPIeFGI": "58-98-62.82%",
+    "ADS-JingJing-1nkPIeFGI": "59-98-62.42%",
     "ADS-JingJing-1ex6GMZPz": "12-19-61.29%",
-    "ADS-JingJing-1gOrKrrC6": "35-53-60.23%",
+    "ADS-JingJing-1gOrKrrC6": "36-53-59.55%",
     "ADS-JingJing-1lvJjnt7a": "18-24-57.14%",
     "ADS-JingJing-1qAqoUTNL": "61-79-56.43%",
     "ADS-JingJing-1h6i9GTga": "106-136-56.20%",
-    "ADS-JingJing-1PJ8YHaHD": "109-134-55.14%",
+    "ADS-JingJing-1PJ8YHaHD": "110-135-55.10%",
+    "ADS-JingJing-1qssklHUM": "60-68-53.13%",
     "ADS-JingJing-1uY2RG4ig": "59-65-52.42%",
-    "ADS-JingJing-1O1oFz5TE": "75-82-52.23%",
-    "ADS-JingJing-1qssklHUM": "60-65-52.00%",
+    "ADS-JingJing-1O1oFz5TE": "76-82-51.90%",
     "ADS-JingJing-1sQ1s48Ea": "40-43-51.81%",
-    "ADS-JingJing-1mFmP5CWj": "150-148-49.66%",
-    "ADS-JingJing-1OzOhLkgG": "48-45-48.39%",
+    "ADS-JingJing-1mFmP5CWj": "152-149-49.50%",
+    "ADS-JingJing-1OzOhLkgG": "49-45-47.87%",
     "ADS-JingJing-1Ib2WT4IO": "24-22-47.83%",
     "ADS-JingJing-1ZPBjXBBg": "19-17-47.22%",
     "ADS-JingJing-1ZbTuMx5p": "50-44-46.81%",
     "ADS-JingJing-1mpCNkcW1": "189-154-44.90%",
-    "ADS-JingJing-1O4ydev9U": "21-15-41.67%",
-    "ADS-JingJing-1IoGxofwY": "157-101-39.15%",
-    "ADS-JingJing-1VX3XgsJL": "21-13-38.24%",
+    "ADS-JingJing-1VX3XgsJL": "21-14-40.00%",
+    "ADS-JingJing-1O4ydev9U": "23-15-39.47%",
+    "ADS-JingJing-1IoGxofwY": "158-103-39.46%",
     "ADS-JingJing-196YKwi0G": "127-73-36.50%",
-    "ADS-JingJing-1emqdFNE9": "58-30-34.09%",
-    "ADS-JingJing-1TqoNxvcQ": "109-54-33.13%",
-    "ADS-JingJing-1f7jjKr0L": "211-101-32.37%",
+    "ADS-JingJing-1emqdFNE9": "58-31-34.83%",
+    "ADS-JingJing-1TqoNxvcQ": "112-55-32.93%",
+    "ADS-JingJing-1f7jjKr0L": "214-102-32.28%",
     "ADS-JingJing-1vu8ScXDy": "106-49-31.61%",
-    "ADS-JingJing-1Ma46oDpt": "24-11-31.43%"
+    "ADS-JingJing-1Ma46oDpt": "25-11-30.56%"
   },
   "FeiFei": {
     "ADS-FeiFei-3ZVt7U34F": "32-39-54.93%",
@@ -63,16 +63,16 @@ var postCNRote = {
   "BeiBei": {
     "ADS-BeiBei-2uwrpFVjG": "17-36-67.92%",
     "ADS-BeiBei-2iLAsExwr": "41-62-60.19%",
-    "ADS-BeiBei-27dnlHvDR": "37-24-39.34%"
+    "ADS-BeiBei-27dnlHvDR": "39-24-38.10%"
   },
   "YingYing": {
-    "ADS-YingYing-6uLVYsdL8": "61-70-53.44%",
-    "ADS-YingYing-6E99QiuJJ": "64-73-53.28%",
+    "ADS-YingYing-6uLVYsdL8": "61-71-53.79%",
+    "ADS-YingYing-6E99QiuJJ": "65-73-52.90%",
     "ADS-YingYing-6h38WAEzz": "149-124-45.42%"
   },
   "GZTianTian": {
     "ADS-GZTianTian-11z39QoYQk": "19-53-73.61%",
-    "ADS-GZTianTian-11jKSYXjGq": "48-54-52.94%"
+    "ADS-GZTianTian-11jKSYXjGq": "48-56-53.85%"
   },
   "KeKe": {
     "ADS-KeKe-9B7rYY4sC": "24-16-40.00%",
@@ -82,7 +82,7 @@ var postCNRote = {
     "ADS-NiNi-7doTDcv5m": "38-39-50.65%"
   },
   "HuanHuan": {
-    "ADS-HuanHuan-5QMTaDhnQ": "67-42-38.53%",
-    "ADS-HuanHuan-5v8i8u76n": "53-25-32.05%"
+    "ADS-HuanHuan-5QMTaDhnQ": "68-42-38.18%",
+    "ADS-HuanHuan-5v8i8u76n": "54-25-31.65%"
   }
 }
