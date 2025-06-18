b80266cae96ae06c35d928f79f7e4a47ac0551fe
klao258 <klao258@users.noreply.github.com>
2025-06-18 18:27:44 +0000

    deploy: 92ab1573055edaefe65790a49a52c048bbf0c1e4

--- autoADS.js
+++ autoADS.js
@@ -18,7 +18,7 @@
     // 封装get请求
     const get = async (path, params = {}) => {
         const query = new URLSearchParams(params).toString();
-        let res = fetch(`http://localhost:3003${path}?${query}`)
+        let res = await fetch(`http://localhost:3003${path}?${query}`)
         console.log(res);
         
         res = res?.data || []
