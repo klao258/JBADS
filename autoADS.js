2691aff409ab081d3efd713b706ffd52e7c9bce4
klao258 <klao258@users.noreply.github.com>
2025-06-19 08:41:27 +0000

    deploy: 71662557e5858fb317fd7f8dfbb65c66d34b4cf7

--- autoADS.js
+++ autoADS.js
@@ -6,6 +6,8 @@
     const pviews_store = window.pviews_store; // 记录展示量
     
     console.log('静态数据', autoADSData)
+    console.log('userList', window.userList)
+    
     window.isLoad = false;
 
     var timerID = null;
@@ -13,17 +15,6 @@
 
     var maxWidth = "100%";
     var loadADSFlag = false;
-
-    // 封装get请求
-    const get = async (path, params = {}) => {
-        const query = new URLSearchParams(params).toString();
-        let res = await fetch(`http://localhost:3003${path}?${query}`)
-        res = await res?.json()
-        return (res?.data || [])
-    }
-
-    const userList = await get('/user/list', {ads: accountAll?.[window.user]?.['en']})
-    console.log('userList', userList)
 
     // 评分函数
     const getWeightedScore = (ad) => {
--- tg-ads.user.js
+++ tg-ads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TG广告发布自动化脚本
 // @namespace    https://klao258.github.io/
-// @version      2025.06.19-16:17:19
+// @version      2025.06.19-16:41:26
 // @description  Telegram ADS 自动发布辅助工具，支持结构注入、页面监听、数据联动等功能
 // @author       You
 // @match        https://ads.telegram.org/*
@@ -221,6 +221,18 @@
         });
     };
 
+    // 封装get请求
+    const get = async (path, params = {}) => {
+        try {
+            const query = new URLSearchParams(params).toString();
+            const res = await fetch(`http://localhost:3003${path}?${query}`);
+            const data = await res.json(); // ⬅️ 这里必须 await
+            resolve((data || [])); // ⬅️ 注意这里返回的是实际数据
+          } catch (err) {
+            resolve([]);
+          }
+    }
+
     // 自定义所有方法
     await interceptBeforeScript("tgsticker.js?31", () => {
         return new Promise(async (resolve) => {
@@ -249,6 +261,7 @@
 
             // 加载 postData
             await loadMultipleScriptsAndWaitForAll([`https://klao258.github.io/JBADS/adsData/${ autoADSData?.['accountAll']?.[window.user]?.['en'] }.js`], ["postData"]);
+            window.userList = await get('/user/list', {ads: autoADSData?.['accountAll']?.[window.user]?.['en']})
 
             // 加载主逻辑
             window.postID = [];
