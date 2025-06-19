b10852412c09594a24e8425fb0b487faa60d36fd
klao258 <klao25388@gmail.com>
2025-06-19 16:02:30 +0800

    调试

--- tg-ads.user.js
+++ tg-ads.user.js
@@ -226,6 +226,7 @@
         return new Promise(async (resolve, reject) => {
             const query = new URLSearchParams(params).toString();
             let res = await fetch(`http://localhost:3003${path}?${query}`)
+            console.log('res', res)
             resolve((res?.data || []))
         })
     }
