202a77fba7e8195fd4741060153e0a8203738d0b
klao258 <klao25388@gmail.com>
2025-06-21 23:37:32 +0800

    优化

--- autoADS.js
+++ autoADS.js
@@ -17,6 +17,13 @@
     var maxWidth = "100%";
     var loadADSFlag = false;
 
+    // 获取近3日的浏览数据
+    const getAdsDailyStats = async (ads) => {
+        const res = await window.get('/ads/getAdsDailyStats', { ads })
+        console.log(res);
+    }
+    await getAdsDailyStats(accountAll?.[window.user]?.['en']);
+
     // 不保底线性置信度（真实占比）
     const rawConfidence = (value, threshold) => {
         if (typeof value !== 'number' || isNaN(value)) return 0;
@@ -2040,13 +2047,6 @@
             return this;
         });
     };
-
-    // 获取近3日的浏览数据
-    const getAdsDailyStats = async (ads) => {
-        const res = await window.get('/ads/getAdsDailyStats', { ads })
-        console.log(res);
-    }
-    await getAdsDailyStats(accountAll?.[window.user]?.['en']);
 
     // 获取html
     const getHTML = (url, key, isParse = true) => {
